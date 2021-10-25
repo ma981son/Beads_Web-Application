b05ccdd800bda7b1715518bab184732988437e7a
ma981son <ma981son@htwg-konstanz.de>
2021-10-25 21:29:45 +0200

    Updated Index + removed huichol page

--- app/controllers/BeadsController.scala
+++ app/controllers/BeadsController.scala
@@ -1,15 +1,15 @@
 package controllers
 
 import javax.inject._
-
 import play.api.mvc._
 import de.htwg.se.beads.Beads
+import de.htwg.se.beads.controller.controllerComponent.ControllerInterface
 
 
 @Singleton
 class BeadsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  val beadController = Beads.controller
-  def beadsAsText = beadController.tempToString
+  val beadController: ControllerInterface = Beads.controller
+  def beadsAsText: String = beadController.tempToString
 
   def about: Action[AnyContent] = Action {
     Ok(views.html.index())
@@ -19,15 +19,11 @@ class BeadsController @Inject()(cc: ControllerComponents) extends AbstractContro
     Ok(views.html.beads(beadController))
   }
 
-  def huichol: Action[AnyContent] = Action {
-    Ok(views.html.huichol())
-  }
-
-  def newTemp = Action {
+  def newTemp: Action[AnyContent] = Action {
     Ok(views.html.beads(beadController))
   }
 
-  def resize(length: Int,width: Int) = Action {
+  def resize(length: Int,width: Int): Action[AnyContent] = Action {
     beadController.changeSize(length,width)
     Ok(views.html.beads(beadController))
   }
